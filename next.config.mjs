b3821b438693172e78d9e3ab7e9e7844f5fa9964
pulkitdigital <pulkitdigitalsolutions@gmail.com>
2025-10-27 12:44:20 +0530

    Fix alias imports with jsconfig; add missing components; node 20 engine

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,29 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // output: "export",
+//   // basePath: process.env.BASE_PATH || "", // Fallback to empty string if BASE_PATH is not set
+//   // assetPrefix:
+//   //   process.env.URL || "https://saddlebrown-cod-609937.hostingersite.com/", // Fallback URL if not set
+//   images: {
+//     unoptimized: true, // Disable image optimization
+//   },
+// };
+
+// export default nextConfig;
+// // (file name)
+// // .env.local
+// // (content inside)
+// //   BASE_PATH=""
+// // URL="https://saddlebrown-cod-609937.hostingersite.com/"
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "export",
-  // basePath: process.env.BASE_PATH || "", // Fallback to empty string if BASE_PATH is not set
-  // assetPrefix:
-  //   process.env.URL || "https://saddlebrown-cod-609937.hostingersite.com/", // Fallback URL if not set
+  // basePath: process.env.BASE_PATH || "",
+  // assetPrefix: process.env.URL || "https://saddlebrown-cod-609937.hostingersite.com/",
   images: {
-    unoptimized: true, // Disable image optimization
+    unoptimized: true,
   },
 };
-
 export default nextConfig;
-// (file name)
-// .env.local
-// (content inside)
-//   BASE_PATH=""
-// URL="https://saddlebrown-cod-609937.hostingersite.com/"
